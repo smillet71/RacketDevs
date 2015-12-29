e32ce85f37734ae71060525ee818164d994e1a5a
smillet <smillet71@free.fr>
2015-12-29 16:28:37 +0100

    ajout test dev world

--- world/world.rkt
+++ world/world.rkt
@@ -1,6 +1,11 @@
 #lang racket
 (require math)
 
+(provide world-position%
+         entity-bounding-box%
+         entity%
+         world%)
+
 ;; ------------------------------------------
 ;; position, attitudes et vitesse d'un objet
 ;; ------------------------------------------
@@ -39,7 +44,7 @@
   (class object%
     ; initialization
     (super-new)
-    (init-field [x 0.0] [y 0.0] [z 0.0])
+    (init-field [x 1.0] [y 1.0] [z 1.0])
     ; accessors
     (define/public (box) (array #[ x y z ] ))))
 
@@ -50,8 +55,9 @@
   (class object%
     ; initialization
     (super-new)
-    (init-field eid etype esubtype eposition ebbox the-world)
+    (init-field etype esubtype eposition ebbox the-world)
     (field [ t 0] ; date
+           [ eid 0 ] ; entity state id
            [ estate 'init ]) ; entity state, 'init 'run 'end)
     ; accessors
     (define/public (id) eid)
@@ -60,10 +66,17 @@
     (define/public (position) eposition)
     (define/public (bbox) ebbox)
     (define/public (state) estate)
+    (define/public (set-id the-eid) (set! eid the-eid))
+
+    ;; add this object to the world 
+    (define/public (add-to-world)
+      (send the-world add-entity this))
+
     ;; initialize 
-    (define/public (initialize)
-      (send the-world add-entity this)
+    (define/public (initialize the-date)
+      (set! t the-date)
       (set! estate 'run))
+      
     ;;
     (define/public (update current-time delta-t) '())
      ))
@@ -77,27 +90,39 @@
     ; initialization
     (super-new)
     (init-field [wname 'the-world] [wid 0] [wseed 0] [wt 0] [cadence-hz 20])
-    (field [ estate 'init ] ; world state, 'init 'run 'end)
-           [ static-entities (make-hash) ] ; hashtable of static entities
+    (field [ estate 'init ]                 ; world state, 'init 'run 'end)
+           [ static-entities (make-hash) ]  ; hashtable of static entities
            [ dynamic-entities (make-hash) ] ; hashtable of dynamic entities
+           [ eids 100]
            )
     ; accessors
     (define/public (name) wname)
     (define/public (id) wid)
     (define/public (seed) wseed)
     (define/public (time) wt)
+    (define/public (ids) eids)
     ; methods
     (define/public (add-entity e)
+      (send e set-id eids)
+      (set! eids (+ eids 1))
       (if (equal? (send e entity-type) 'static)
           (hash-set! static-entities (send e id) e)
           (hash-set! dynamic-entities (send e id) e)))
+    
+    ; initialize all objects added to world
+    (define/public (init)
+      ;
+      (writeln (hash-count static-entities))
+      (for ([key (hash-keys static-entities)])
+        (let ([entity (hash-ref! static-entities key '())])
+          (send entity initialize wt)))
+      ;
+      (writeln (hash-count dynamic-entities))
+      (for ([key (hash-keys dynamic-entities)])
+        (let ([entity (hash-ref! dynamic-entities key '())])
+          (send entity initialize wt))))
+          
     (define/public (update)
       '())
     ))
-
-;; ------------------------------------------
-;; some tests
-;; ------------------------------------------
-(define w (new world%))
-(define e1 (new entity% 1 
 
